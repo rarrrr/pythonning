d4facdc8c1ff14d6cb8b7750655fbaf4f7feaa34
rarrrr <madeleine.mccallum@gmail.com>
2017-07-31 11:18:01 +0100

    week 2 exercises

--- grades.py
+++ grades.py
@@ -1,13 +1,21 @@
-grades = [55, 67, 67, 88, 93, 97]
+#grades = [55, 67, 67, 88, 93, 97]
 #micaela's solution:
 #define a function
-def find_largest(nums):
-    p = nums
+#def find_largest(nums):
+#    p = nums
 #or p = nums[0]
-    for element in nums:
-    if element > p:
-        p = element
-    return p
+#    for element in nums:
+#    if element > p:
+#        p = element
+#    return p
 
-print find_largest(nums)
-    
+#print find_largest(nums)
+
+grades = [67, 55, 97, 88, 93, 67]
+grades.sort();
+
+bottom = grades[0]
+top = grades[-1]
+
+print "bottom is", bottom
+print "top is", top
